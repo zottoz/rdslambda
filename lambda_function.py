dbb3d105d1d0be3e5233bf295b37f5536b8317e8
zottoz <zottozbr@gmail.com>
2021-12-19 16:25:18 -0400

    at..

--- lambda_function.py
+++ lambda_function.py
@@ -2,10 +2,10 @@ import json
 import pymysql
 
 
-RDS_endpoint = "mydatabaseinstance.ckzckyzedeqe.us-east-1.rds.amazonaws.com"
-UserName = "mydatabaseuser"
-Password = "mydatabasepassword"
-DatabaseName = "students"
+RDS_endpoint = "my-db-1.c1tumcbllwas.us-east-1.rds.amazonaws.com"
+UserName = "admin"
+Password = "admin123"
+DatabaseName = "dados"
 
 connectionString = pymysql.connect(RDS_endpoint, user= UserName, passwd= Password, db= DatabaseName)
 
@@ -16,9 +16,9 @@ def lambda_handler(event, context):
     connectionString = pymysql.connect(RDS_endpoint, user= UserName, passwd= Password, db= DatabaseName)
     pointer = connectionString.cursor()
     pointer2 = connectionString.cursor()
-    pointer.execute('SELECT * FROM students')
+    pointer.execute('SELECT * FROM alunos')
     
-    pointer2.execute('DESCRIBE students')
+    pointer2.execute('DESCRIBE alunos')
     #print(pointer2.fetchall())
     
     table_head = pointer2.fetchall()
@@ -31,12 +31,12 @@ def lambda_handler(event, context):
     
     for r in table_rows:
         row_json.append({header[0] : r[0], header[1] : r[1], header[2] : r[2]})
-    table_json = {"student" : row_json}
+    table_json = {"alunos" : row_json}
     for r in table_rows:
         print("---------------------------------")
-        print("Student Name : {}".format(r[0]))
-        print("Course : {}".format(r[1]))
-        print("Semester : {}".format(r[2]))
+        print("ID : {}".format(r[0]))
+        print("Nome : {}".format(r[1]))
+        print("Matricula : {}".format(r[2]))
         print("---------------------------------")
     
     return table_json
